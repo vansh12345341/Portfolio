b2cf2da3b46d4707a8c6cf3fb96d3584ec9a207a
Vansh <vansh432.sharma@gmail.com>
2023-03-14 13:04:56 +0530

    update

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -3,15 +3,24 @@
 export const Work = [
     {
         id:1,
-        name:"Zomato Data Analysis",
-        description:"Zomato Data Analysis using python and visualization libraries",
+        name:"Movie Recommendation System",
+        description:" movie recommendation project involved using data analysis and prediction techniques on an IMDb movie dataset",
         tags:["python","numpy&pandas", "matplotlib"],
         
-        demo:"https://github.com/vansh12345341/Zomato__EDA/blob/main/Zomato%20Analysis.ipynb",
-        github:"https://github.com/vansh12345341/Zomato__EDA"
+        demo:"https://github.com/vansh12345341/Movie-Recommendation-System",
+        github:"https://github.com/vansh12345341/Movie-Recommendation-System"
     },
     {
         id:2,
+        name:"Cricket Data Analysis and Visualization",
+        description:"Cricket T20 World cup Data Analysis using Power BI and web scarpping",
+        tags:["python","numpy&pandas", "machinelearning"],
+        
+        demo:"https://github.com/vansh12345341/Zomato__EDA/blob/main/Zomato%20Analysis.ipynb",
+        github:"https://github.com/vansh12345341/T20-World-cup-cricket-Data-Analytics"
+    },
+    {
+        id:3,
         name:"Olympics Data Analysis",
         description:"Finding insights on Past olympics dataset using python",
         tags:["python","streamlit"],
@@ -21,7 +30,7 @@ export const Work = [
         
     },
     {
-        id:3,
+        id:4,
         name:"Tableua Dashboards",
         description:"Creative Dashboards made using different datasets through tableua",
         tags:["taleua", "excel","design"],
@@ -30,15 +39,26 @@ export const Work = [
         github:"https://public.tableau.com/app/profile/vansh.sharma3095"
     },
     {
-        id:4,
-        name:"Item Menu",
-        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        id:5,
+        name:"DSA Tracker ",
+        description:"Interactive DSA Tracker made using react to track DSA question progress",
         tags:["react","styledComponents","react-router"],
         
-        demo:"https://item-menu.vercel.app/",
-        github:"https://github.com/vansh12345341/Item-menu-"
+        demo:"https://dsa-tracker-beige.vercel.app/",
+        github:"https://github.com/vansh12345341/DSA-Tracker"
        
+    }, 
+    {
+        id:6,
+        name:"Fath Finding Visualizer",
+        description:" pathfinding visualizer application utilizes BFS , BDS algorithms to find the shortest path between two points on a grid",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://path-visualizer-eight.vercel.app/",
+        github:"https://github.com/vansh12345341/Path-Visualizer"
     },
+       
+    
     
   
 ] 
