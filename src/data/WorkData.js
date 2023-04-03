522f2f92f93bd66a3191ed5b288897b876b32787
Vansh <vansh432.sharma@gmail.com>
2023-04-03 20:17:29 +0530

    new

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -12,34 +12,26 @@ export const Work = [
     },
     {
         id:2,
-        name:"Cricket Data Analysis and Visualization",
-        description:"Cricket T20 World cup Data Analysis using Power BI and web scarpping",
-        tags:["python","numpy&pandas", "machinelearning"],
+        name:"Path Finding Visualizer",
+        description:"Path Finding Visualizer made using BFS , BDS and React",
+        tags:["React","Algorithms", "CSS"],
         
-        demo:"https://github.com/vansh12345341/Zomato__EDA/blob/main/Zomato%20Analysis.ipynb",
-        github:"https://github.com/vansh12345341/T20-World-cup-cricket-Data-Analytics"
+        demo:"https://path-visualizer-eight.vercel.app/",
+        github:"https://github.com/vansh12345341/Path-Visualizer"
     },
     {
         id:3,
-        name:"Olympics Data Analysis",
-        description:"Finding insights on Past olympics dataset using python",
-        tags:["python","streamlit"],
+        name:"Real Estate Web Application",
+        description:"Real Estate web app made using Rapid API and Next JS",
+        tags:["Nextjs","API"],
         
-        demo:"https://github.com/vansh12345341/Olympics-Data-Analysis/blob/main/README.md",
-        github:"https://github.com/vansh12345341/Olympics-Data-Analysis"
+        demo:"https://estate-green.vercel.app/",
+        github:"https://github.com/vansh12345341/Estate"
         
     },
+
     {
         id:4,
-        name:"Tableua Dashboards",
-        description:"Creative Dashboards made using different datasets through tableua",
-        tags:["taleua", "excel","design"],
-        
-        demo:"https://public.tableau.com/app/profile/vansh.sharma3095",
-        github:"https://public.tableau.com/app/profile/vansh.sharma3095"
-    },
-    {
-        id:5,
         name:"DSA Tracker ",
         description:"Interactive DSA Tracker made using react to track DSA question progress",
         tags:["react","styledComponents","react-router"],
@@ -49,14 +41,24 @@ export const Work = [
        
     }, 
     {
-        id:6,
-        name:"Fath Finding Visualizer",
-        description:" pathfinding visualizer application utilizes BFS , BDS algorithms to find the shortest path between two points on a grid",
-        tags:["react","styledComponents","react-router"],
+        id:5,
+        name:"Expense Tracker",
+        description:"Expense Tracker made using React and context API",
+        tags:["react","State management","CSS"],
         
-        demo:"https://path-visualizer-eight.vercel.app/",
-        github:"https://github.com/vansh12345341/Path-Visualizer"
+        demo:"https://expense-tracker-six-woad.vercel.app/",
+        github:"https://github.com/vansh12345341/Expense-Tracker"
     },
+    {
+        id:6,
+        name:"T20 World cup data analysis",
+        description:"World cup data analysis and visualization",
+        tags:["Python","Power BI","WebScraping"],
+        
+        demo:"https://github.com/vansh12345341/T20-World-cup-cricket-Data-Analytics",
+        github:"https://github.com/vansh12345341/T20-World-cup-cricket-Data-Analytics"
+    }
+       
        
     
     
